ba733099e33848e27c8fd9f233f7d11b368d5ff9
aacevedoday <aacevedo@dayscript.com>
2018-06-12 18:30:41 -0500

    update

--- resources/assets/js/components/predictions/PredictionsIndexTree.vue
+++ resources/assets/js/components/predictions/PredictionsIndexTree.vue
@@ -114,7 +114,12 @@
                 </div>
             </div>
             <div class="text-center margin-30">
-                <img src="/img/banners/MPI_320x50_Q20089_GM_99Coverage_ST.jpg" alt="" class="Add">
+                <!-- <img src="/img/banners/MPI_320x50_Q20089_GM_99Coverage_ST.jpg" alt="" class="Add"> -->
+                <div id='div-gpt-ad-1528227919403-0' style="width: 728px; height: 90px">
+                  <script>
+                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1528227919403-0'); });
+                  </script>
+                </div>
             </div>
         </div>
     </div>
