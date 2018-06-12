f8fd6a908bb0f33e14ba5fe6085f20aeca2809ea
aacevedoday <aacevedo@dayscript.com>
2018-06-12 18:35:34 -0500

    update

--- resources/views/pages/ranking.blade.php
+++ resources/views/pages/ranking.blade.php
@@ -11,3 +11,8 @@
         <ranking-index :rounds="{{ $rounds }}" :leagues="{{ $leagues }}" userid="{{ auth()->user()->id }}" ></ranking-index>
     @endif
 @endsection
+<div id='div-gpt-ad-1528227919403-0' style="width: 728px; height: 90px">
+  <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1528227919403-0'); });
+  </script>
+</div>
