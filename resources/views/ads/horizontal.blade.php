6ec413ac2cb4a00bbc55a0fe5c8e4b2985a44e07
aacevedoday <aacevedo@dayscript.com>
2018-06-12 18:45:29 -0500

    update

--- resources/views/ads/horizontal.blade.php
+++ resources/views/ads/horizontal.blade.php
@@ -1,6 +1,6 @@
 <!-- <img src="{{ asset('img/banners/MPI_320x50_Q20089_GM_99Coverage_ST.jpg') }}" alt="" class="Add"> -->
 
-<div id='div-gpt-ad-1528227919403-0' style="width: 300px; height: 600px">
+<div id='div-gpt-ad-1528227919403-0' style="width: 798px; height: 90px">
   <script>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1528227919403-0'); });
   </script>
