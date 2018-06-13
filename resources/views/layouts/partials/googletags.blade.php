6e0bcf7263927e2e943ceca8344555f4ae15bfea
aacevedoday <aacevedo@dayscript.com>
2018-06-13 09:47:37 -0500

    update

--- resources/views/layouts/partials/googletags.blade.php
+++ resources/views/layouts/partials/googletags.blade.php
@@ -6,7 +6,17 @@
 
 <script>
   googletag.cmd.push(function() {
-    googletag.defineSlot('\/6881\/rd.univision_section_deportes\/futbol\/mundialrusia2018\/predictordemisionrusia', [[ 300.0 ,  250.0 ], [ 300.0 ,  600.0 ]], [[ 728.0 ,  90.0 ]], 'div-gpt-ad-1528227919403-0').addService(googletag.pubads());
+    googletag.defineSlot('\/6881\/rd.univision_section_deportes\/futbol\/mundialrusia2018\/predictordemisionrusia', [[ 300.0 ,  250.0 ], [ 300.0 ,  600.0 ]], 'div-gpt-ad-1528227919403-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().collapseEmptyDivs();
+    googletag.enableServices();
+  });
+</script>
+
+
+<script>
+  googletag.cmd.push(function() {
+    googletag.defineSlot('\/6881\/rd.univision_section_deportes\/futbol\/mundialrusia2018\/predictordemisionrusia', [[ 728.0 ,  90.0 ]], 'div-gpt-ad-1528227919403-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
     googletag.enableServices();
