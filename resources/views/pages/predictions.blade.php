ca57c98fe17c581f63e86d4b040fe8a71df74f74
aacevedoday <aacevedo@dayscript.com>
2018-06-13 09:06:28 -0500

    update

--- resources/views/pages/predictions.blade.php
+++ resources/views/pages/predictions.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="page">
   <predictions-index :groups="{{ $groups }}"></predictions-index>
-  <div class="row" style="text-align: center">
+  <div class="row" style="text-align: center;padding-bottom: 15px">
       <img src="/img/banners/MPI_320x50_Q20089_GM_99Coverage_ST.jpg" alt="" class="Add">
   </div>
 </div>
