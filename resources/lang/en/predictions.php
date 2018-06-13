0b5a561413e8875e4f68535676ebc8ffc8610296
aacevedoday <aacevedo@dayscript.com>
2018-06-13 17:33:22 -0500

    lang image

--- resources/lang/en/predictions.php
+++ resources/lang/en/predictions.php
@@ -25,6 +25,6 @@ return [
     'predictions_help_message'=> 'Group A is near closing: You can make changes to your predictions until 15 minutes before the Uruguay vs Rusia match on Jun 25th',
     'total_points'            => 'Total score',
     'win_in_these_phase'      => 'Win in this group phase up to $3,000',
-    'share_image'             => 'prepredictor-social-en',
+    'share_image'             => 'predictor-social-en',
 
 ];
