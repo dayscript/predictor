396fa535212899f1f95ccc87db0f0c72a17583a9
aacevedoday <aacevedo@dayscript.com>
2018-06-13 08:46:51 -0500

    update

--- resources/views/pages/predictions.blade.php
+++ resources/views/pages/predictions.blade.php
@@ -6,20 +6,6 @@
 
 @section('content')
 <div class="page">
-  <div class="section">
-      <div class="row">
-          <div class="medium-8 columns text-left">
-              <div class="title-section">
-                  {{ __('menu.my_predictions') }}
-              </div>
-          </div>
-          <div class="medium-4 columns">
-              <div class="alerta" v-if="updated">
-                  {{ __('predictions.predictions_updated') }}
-              </div>
-          </div>
-      </div>
-  </div>
-  <predictions-index :groups="{{ $groups }}"></predictions-index>
+    <predictions-index :groups="{{ $groups }}"></predictions-index>
 </div>
 @endsection
