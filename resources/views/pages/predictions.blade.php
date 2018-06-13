ef2cf6fec7e3256729b513e769bbed3b5ff99f8e
aacevedoday <aacevedo@dayscript.com>
2018-06-13 09:07:02 -0500

    update

--- resources/views/pages/predictions.blade.php
+++ resources/views/pages/predictions.blade.php
@@ -9,6 +9,7 @@
   <predictions-index :groups="{{ $groups }}"></predictions-index>
   <div class="row" style="text-align: center;padding-bottom: 15px">
       <img src="/img/banners/MPI_320x50_Q20089_GM_99Coverage_ST.jpg" alt="" class="Add">
+      @include('ads.horizontal')
   </div>
 </div>
 @endsection
