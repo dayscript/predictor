155256bfffc45d09d121165a81e6ad4b35789548
aacevedoday <aacevedo@dayscript.com>
2018-06-13 09:58:51 -0500

    update

--- resources/views/layouts/partials/googletags.blade.php
+++ resources/views/layouts/partials/googletags.blade.php
@@ -3,7 +3,7 @@
   var googletag = googletag || {};
   googletag.cmd = googletag.cmd || [];
 </script>
-
+@if(Request::url() !== 'predictions')
 <script>
   googletag.cmd.push(function() {
     googletag.defineSlot('\/6881\/rd.univision_section_deportes\/futbol\/mundialrusia2018\/predictordemisionrusia', [[ 300.0 ,  250.0 ], [ 300.0 ,  600.0 ]], 'div-gpt-ad-1528227919403-0').addService(googletag.pubads());
@@ -12,13 +12,16 @@
     googletag.enableServices();
   });
 </script>
+@endif
 
 
-<script>
-  googletag.cmd.push(function() {
-    googletag.defineSlot('\/6881\/rd.univision_section_deportes\/futbol\/mundialrusia2018\/predictordemisionrusia', [[ 728.0 ,  90.0 ]], 'div-gpt-ad-1528227919403-0').addService(googletag.pubads());
-    googletag.pubads().enableSingleRequest();
-    googletag.pubads().collapseEmptyDivs();
-    googletag.enableServices();
-  });
-</script>
+@if(Request::url() === 'predictions')
+  <script>
+    googletag.cmd.push(function() {
+      googletag.defineSlot('\/6881\/rd.univision_section_deportes\/futbol\/mundialrusia2018\/predictordemisionrusia', [[ 728.0 ,  90.0 ]], 'div-gpt-ad-1528227919403-0').addService(googletag.pubads());
+      googletag.pubads().enableSingleRequest();
+      googletag.pubads().collapseEmptyDivs();
+      googletag.enableServices();
+    });
+  </script>
+@endif
