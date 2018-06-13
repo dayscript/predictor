a0b138aef2371289acda1aab71543a602f5e7ffb
aacevedoday <aacevedo@dayscript.com>
2018-06-13 08:45:40 -0500

    update

--- resources/views/pages/predictions.blade.php
+++ resources/views/pages/predictions.blade.php
@@ -5,8 +5,21 @@
 @endsection
 
 @section('content')
-    <predictions-index :groups="{{ $groups }}"></predictions-index>
-    <div class="medium-12 text-center banner ranking">
-        @include('ads.horizontal')
-    </div>
+<div class="page">
+  <div class="section">
+      <div class="row">
+          <div class="medium-8 columns text-left">
+              <div class="title-section">
+                  {{ __('menu.my_predictions') }}
+              </div>
+          </div>
+          <div class="medium-4 columns">
+              <div class="alerta" v-if="updated">
+                  {{ __('predictions.predictions_updated') }}
+              </div>
+          </div>
+      </div>
+  </div>
+  <predictions-index :groups="{{ $groups }}"></predictions-index>
+</div>
 @endsection
