2fde40e83d92b85581505fe6baf820e3bb4b758c
aacevedoday <aacevedo@dayscript.com>
2018-06-12 19:27:19 -0500

    update

--- resources/views/pages/ranking.blade.php
+++ resources/views/pages/ranking.blade.php
@@ -5,12 +5,28 @@
 @endsection
 
 @section('content')
-    @if($l)
-        <ranking-index :rounds="{{ $rounds }}" :leagues="{{ $leagues }}" userid="{{ auth()->user()->id }}" :l="{{ $l }}"></ranking-index>
-    @else
-        <ranking-index :rounds="{{ $rounds }}" :leagues="{{ $leagues }}" userid="{{ auth()->user()->id }}" ></ranking-index>
-    @endif
+
+<div class="page">
+    <div class="section">
+        <div class="row">
+            <div class="medium-12 columns text-left">
+                <div class="title-section">
+                    {{ $store.getters.trans('menu.ranking') }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="padding-30">
+        <div class="row">
+        @if($l)
+            <ranking-index :rounds="{{ $rounds }}" :leagues="{{ $leagues }}" userid="{{ auth()->user()->id }}" :l="{{ $l }}"></ranking-index>
+        @else
+            <ranking-index :rounds="{{ $rounds }}" :leagues="{{ $leagues }}" userid="{{ auth()->user()->id }}" ></ranking-index>
+        @endif
+      </div>
+    </div>
     <div class="medium-3 columns text-center banner">
         @include('ads.vertical')
     </div>
+</div>
 @endsection
