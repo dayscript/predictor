f1ddf1e7add93895f9d986208211f88adf755051
aacevedoday <aacevedo@dayscript.com>
2018-06-14 13:01:32 -0500

    ads goolge vue component

---
+++ resources/assets/js/components/ads/AdsGoogle.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id='div-gpt-ad-1528227919403-0'>
+  </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1528227919403-0'); });
+        }
+    }
+</script>
