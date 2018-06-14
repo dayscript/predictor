7bf37cfbf4051cfd0e492df771b0d9b36302bfd9
aacevedoday <aacevedo@dayscript.com>
2018-06-14 13:16:48 -0500

    ads goolge vue component

--- resources/views/ads/horizontal.blade.php
+++ resources/views/ads/horizontal.blade.php
@@ -1,12 +1,3 @@
 <!-- /6881/rd.univision_section_deportes/futbol/mundialrusia2018/predictordemisionrusia -->
 
-<div id='div-gpt-ad-1528227919403-0'>
-
-<script>
-
-googletag.cmd.push(function() { googletag.display('div-gpt-ad-1528227919403-0'); });
-
-</script>
-
-</div>
-   
+<ads-google></ads-google>
