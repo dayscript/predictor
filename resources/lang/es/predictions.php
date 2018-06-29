7e1716d73efcda683581dcd46f0ee084a85f6b67
jcardenas2015 <jcardenas@dayscript.com>
2018-06-29 12:06:10 -0500

    ajuste tab default

--- resources/lang/es/predictions.php
+++ resources/lang/es/predictions.php
@@ -22,8 +22,8 @@ return [
     'share_group_description'  => 'Tu también puedes participar y ganar fabulosos premios',
     'share_match_title'        => 'Este es mi pronóstico para el partido :match del Mundial de Rusia 2018',
     'share_match_description'  => 'Tu también puedes participar y ganar fabulosos premios',
-    'predictions_help_message' => 'La Fase de grupos ha cerrado sus pronósticos',
+    'predictions_help_message' => 'Estan abiertos los pronosticos para la fase Octavos de Final',
     'total_points'             => 'Puntaje acumulado',
-    'win_in_these_phase'       => 'Gana en esta fase de grupos hasta $3,000',
+    'win_in_these_phase'       => 'Gana en esta fase de octavos hasta $2,000',
     'share_image'              => 'predictor-social-es',
 ];
